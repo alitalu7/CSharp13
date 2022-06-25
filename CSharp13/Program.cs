9e426ebe04db9d5e383d2e40292e3fd4be6a14ef
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-25 14:03:36 +0300

    Add project files.

---
+++ CSharp13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Bir sayı giriniz...");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(Fibo(i));
+}
+
+static int Fibo(int x)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+    else if (x == 1)
+    {
+        return 1;
+    }
+    return Fibo(x - 1) + Fibo(x - 2);
+}
+
+
